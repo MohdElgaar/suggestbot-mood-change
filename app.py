a47ee32236f87a2c05e66ce563444954fb9019b2
Jino-Y <63004784+Jino-Y@users.noreply.github.com>
2020-05-27 18:03:58 +0900

    Add files via upload
    pie chart added ver2

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pandas as pd
 from datetime import date, time, datetime
 
 app = dash.Dash(__name__)
-server = app.server
 
 placeholder_data = pd.DataFrame({'action': ['Facebook (App)', 'Call',
     'Weather Change', 'Exercise', 'Youtube (App)', 'Netflix (App)', 'Extra'],
@@ -15,9 +14,7 @@ placeholder_data = pd.DataFrame({'action': ['Facebook (App)', 'Call',
     'time': [time(13, 00), time(15, 0), time(12, 0), time(11, 0),
         time(19, 0), time(17, 0), None],
     'icon': ['fb.png', 'phone.png', 'weather.png', 'workout.png',
-        'yt.png', 'netflix.png', None],
-    'effects': [4, 2, 1, 3, 5, 1, 2]
-        })
+        'yt.png', 'netflix.png', None]})
 
 placeholder_data_line = pd.DataFrame({
     'time': [time(i) for i in range(9,22)],
@@ -52,7 +49,7 @@ def line_plot(df_line, df_data, UV = False):
     min_time = 9
     max_valence = max(df_line['valence'])
     min_valence = -5
-
+    
     layout = {'images': [],
             'paper_bgcolor': 'lightgray'}
     for _, item in df_data.iterrows():
@@ -91,20 +88,6 @@ def line_plot(df_line, df_data, UV = False):
     else:
         return {'data': [valence], 'layout': layout}
 
-
-def pie_chart(df, selected = 'Pos'):
-    colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen']
-    chosen = df[df['type'] == selected]
-    label = chosen['action']
-    value = chosen['effects']
-    pie = go.Pie(labels=label, values=value, name = "pie")
-    fig = go.Figure(data=[pie])
-    fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
-                      marker=dict(colors=colors, line=dict(color='#000000', width=2)))
-    return fig
-
-
-
 app.layout = html.Div(id='bottom', children = [
     html.Div(className = 'right', children = [
         html.Table(className = 'content-table', children = [
@@ -112,33 +95,24 @@ app.layout = html.Div(id='bottom', children = [
                 html.Tr([
                     # html.Th(id='pos', children = ["Positive"]),
                     html.Th([
-                    html.Button("Positive", id='Pos', n_clicks=0),
+                    html.Button("Positive", id = 'pos'),
                     ]),
                     # html.Th(id='neg', children = ["Negative"])
                     html.Th([
-                    html.Button("Negative", id='Neg', n_clicks=0)
+                    html.Button("Negative", id = 'neg')
                     ]),
                 ])
                 ]),
             html.Tbody(fill_table(placeholder_data))
             ])
         ]),
-    html.Div(id='line1', className = 'left', children = [
+    html.Div(className = 'left', children = [
         html.H1("Positive & Negative Mood Influencers"),
         html.Div(id='plot', children=[dcc.Graph(id = 'line_plot')]),
         html.Center([html.Button("Show UV Exposure", id = "UV_button",
             n_clicks = 0)])
-        ]),
-
-
-    html.Div(id='pie1', className='left', children=[dcc.Graph(id='pie-chart1')]
-             , style={'display': 'none'}),
-    html.Div(id='pie2', className='left', children=[dcc.Graph(id='pie-chart2')]
-             , style={'display': 'none'})
-
-
+        ])
     ])
-
 
 @app.callback(
         Output("line_plot", 'figure'), [Input("UV_button", "n_clicks")]
@@ -148,28 +122,6 @@ def callback(n_clicks):
         return line_plot(placeholder_data_line, placeholder_data, UV=False)
     else:
         return line_plot(placeholder_data_line, placeholder_data, UV=True)
-
-
-@app.callback(
-   [Output('pie1', 'style'), Output('pie-chart1', 'figure')],
-   [Input('Pos', 'n_clicks')])
-def pospie(n_clicks):
-    if n_clicks % 2 == 0:
-        return {'display': 'none'}, pie_chart(placeholder_data)
-    else:
-        return {'display': 'block'}, pie_chart(placeholder_data, 'Pos')
-
-
-@app.callback(
-   [Output('pie2', 'style'), Output('pie-chart2', 'figure')],
-   [Input('Neg', 'n_clicks')])
-def negpie(n_clicks):
-    if n_clicks % 2 == 0:
-        return {'display': 'none'}, pie_chart(placeholder_data, 'Neg')
-    else:
-        return {'display': 'block'}, pie_chart(placeholder_data, 'Neg')
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
