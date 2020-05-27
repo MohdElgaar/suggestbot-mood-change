5065d1953e6085650ee5243e75bb31d29bf9fdb8
Mohamed Elgaar <mohamed@kaist.ac.kr>
2020-05-27 17:17:42 +0900

    add server

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 from datetime import date, time, datetime
 
 app = dash.Dash(__name__)
+server = app.server
 
 placeholder_data = pd.DataFrame({'action': ['Facebook (App)', 'Call',
     'Weather Change', 'Exercise', 'Youtube (App)', 'Netflix (App)', 'Extra'],
